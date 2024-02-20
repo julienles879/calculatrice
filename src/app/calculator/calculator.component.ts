feaafdb3a8e74dc48f108930965f013f65fbfc8d
julienles879 <julienlesimple1996@gmail.com>
2024-02-20 12:05:54 +0100

    front base

--- src/app/calculator/calculator.component.ts
+++ src/app/calculator/calculator.component.ts
@@ -5,7 +5,7 @@ import { Component } from '@angular/core';
   standalone: true,
   imports: [],
   templateUrl: './calculator.component.html',
-  styleUrl: './calculator.component.css'
+  styleUrl: './calculator.component.scss'
 })
 export class CalculatorComponent {
 
