fc9ef0f7da052a04c6a608aebdbce7278b0668e2
julienles879 <julienlesimple1996@gmail.com>
2024-02-20 16:03:07 +0100

    calcule OK

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,16 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { CalculatorComponent } from './calculator/calculator.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  imports: [
+    CalculatorComponent,
+  ],
+  templateUrl: 'app.component.html',
+  styleUrl: 'app.component.scss'
 })
+
 export class AppComponent {
-  title = 'calculatrice';
+
 }
--- src/app/calculator/calculator.component.ts
+++ src/app/calculator/calculator.component.ts
@@ -1,12 +1,52 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-calculator',
   standalone: true,
-  imports: [],
+  imports: [
+    FormsModule
+  ],
   templateUrl: './calculator.component.html',
-  styleUrl: './calculator.component.scss'
+  styleUrls: ['./calculator.component.scss']
 })
-export class CalculatorComponent {
 
+export class CalculatorComponent {
+  calcul: string = '';
+  result: string = '';
+  unite: string = '';
+
+  constructor() {
+    const storedInput = localStorage.getItem('currentInput');
+    if (storedInput) {
+      this.calcul = storedInput;
+    }
+  }
+
+  ajouterAuCalcul(value: string) {
+    if (value === '=') {
+      console.log('Évaluation du calcul...');
+      this.evaluerCalcul();
+    } else if (value === 'C') {
+      console.log('Effacement de la saisie...');
+      this.calcul = '';
+      localStorage.removeItem('currentInput');
+    } else {
+      console.log('Ajout de la valeur ' + value + ' à la saisie...');
+      this.calcul += value;
+      localStorage.setItem('currentInput', this.calcul);
+    }
+  }
+
+  evaluerCalcul() {
+    try {
+      console.log('Évaluation de ' + this.calcul + '...');
+      this.result = eval(this.calcul);
+      console.log('Résultat : ' + this.result);
+      localStorage.setItem('currentInput', this.result);
+    } catch (e) {
+      console.log('Une erreur est survenue : ' + e);
+      this.result = 'Erreur';
+    }
+  }
 }
